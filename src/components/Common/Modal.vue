dda88c32de67390e9e4dc02a7476dfe6c9a1e82b
MaryamTavana379 <maryam.tavana379@gmail.com>
2024-01-31 18:04:59 +0330

    Create rangebar and checkbox components for filter section

--- src/components/Common/Modal.vue
+++ src/components/Common/Modal.vue
@@ -1,6 +1,37 @@
 <script setup>
 import IconButton from "@cp/Common/IconButton.vue";
-import slider from "vue3-slider"
+import slider from "vue3-slider";
+import Checkbox from "@cp/Layout/Home/Checkbox.vue";
+
+const data = {
+  all:{
+    title:'همه',
+    items:[
+      {id:1, txt:'جدیدترین ها', name:'newest'},
+      {id:2, txt:'پرفروش ترین ها', name:'bestSellers'},
+      {id:3, txt:' گران ترین ها', name:'expensive'},
+      {id:4, txt:'ارزان ترین ها', name:'cheap'},
+      {id:5, txt:'بیشترین امتیاز', name:'score'},
+    ]
+  },
+  season:{
+    className:'season-row',
+    title: 'بر اساس فصل',
+    items:[
+      {id:1, txt:' بهاری', name:'spring'},
+      {id:2, txt:'  تابستانی', name:'summer'},
+      {id:3, txt:'   پاییزی', name:'autumnal'},
+      {id:4, txt:'  زمستانی', name:'Wintry'},
+    ]
+  },
+  price:{
+    isVisible:true,
+    className:'season-row',
+    title:'بر اساس قیمت',
+  }
+
+}
+
 </script>
 
 <template>
@@ -12,32 +43,17 @@ import slider from "vue3-slider"
         <IconButton img="icon/home/close.svg" @click="$emit('onClose')"/>
       </div>
       <section class="modal__container--body">
-        <div class="modal__container--body-row">
-          <div class="row__title">
-            <span>  همه </span>
+        <div class="modal__container--body-row" v-for="item in data" >
+          <div class="row__title" :class="item.className">
+            <span>  {{item.title}} </span>
             <hr/>
           </div>
-          <div class="row__container">
-            <div class="row__container--items">
-              <input type="checkbox" id="newest" />
-              <label for="newest">جدیدترین ها</label>
-            </div>
-            <div class="row__container--items">
-              <input type="checkbox" id="newest" />
-              <label for="newest">جدیدترین ها</label>
-            </div>
-          </div>
-
+          <section class="row__container">
+            <Checkbox :data="item.items"/>
+          </section>
+          <slider v-model="myNumber" step="2" v-if="item.isVisible"/>
         </div>
-        <div class="modal__container--body-row"></div>
-        <div class="modal__container--body-row">
-          <slider v-model="myNumber" step="2" />
-        </div>
-
       </section>
-
-
-
     </div>
   </div>
 
---
+++ src/components/Layout/Home/Checkbox.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['data']);
+</script>
+
+<template>
+  <div class="items" v-for="item in data" >
+    <div class="items-card">
+      <input type="checkbox" :id="item.name" />
+      <label :for="item.name"> {{item.txt}}</label>
+    </div>
+  </div>
+
+</template>
+
